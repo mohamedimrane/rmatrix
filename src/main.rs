83bed75543b33d19ef33978a2723d776b892b588
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-08-02 18:46:47 +0100

    Add basic matrix effect

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use rand::{seq::SliceRandom, Rng};
-use std::io::Write;
-use termion::raw::IntoRawMode;
+use std::io::{Stdout, Write};
+use termion::{
+    cursor::DetectCursorPos,
+    raw::{IntoRawMode, RawTerminal},
+};
 
 const CHARACTERS: &[char] = &[
     'ｦ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｬ', 'ｭ', 'ｮ', 'ｯ', 'ｰ', 'ｱ', 'ｲ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ',
@@ -15,27 +18,44 @@ const CHARACTERS: &[char] = &[
 const COLOR: termion::color::Rgb = termion::color::Rgb(46, 248, 47);
 
 struct Drop {
-    lenght: u16,
+    length: u16,
     x_pos: u16,
+    y_offset: u16,
     speed: std::time::Duration,
     next_time: std::time::Instant,
     characters: Vec<char>,
 }
 
 impl Drop {
-    fn draw(&mut self) {
-        for (index, c) in self.characters.iter().enumerate() {
-            draw(*c, (self.x_pos, (index + 1) as u16), COLOR);
+    fn draw_if_timer_is_elapsed(&mut self, stdout: &mut RawTerminal<Stdout>) {
+        if std::time::Instant::now() >= self.next_time {
+            print!("{}", termion::cursor::Goto(self.x_pos, self.y_offset));
+            let (x, y) = stdout.cursor_pos().unwrap();
+            write!(stdout, "{}{}", " ", termion::cursor::Goto(x - 1, y));
+
+            for (index, c) in self.characters.iter().enumerate() {
+                let iu16 = index as u16;
+                if iu16 < self.y_offset || iu16 > self.length + self.y_offset {
+                    continue;
+                }
+
+                let offset = self.y_offset as usize + 1;
+                draw(*c, (self.x_pos, (index + offset) as u16), COLOR);
+            }
+
+            self.y_offset += 1;
+
+            self.reset_timer();
         }
     }
 
     fn reset_timer(&mut self) {
-        self.next_time = std::time::Instant::now() + self.speed;
+        self.next_time += self.speed;
     }
 }
 
 fn main() {
-    let _stdout = std::io::stdout().into_raw_mode().unwrap();
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
     let terminal_size = termion::terminal_size().unwrap();
 
     print!(
@@ -49,7 +69,8 @@ fn main() {
 
     let mut next_time_spawn = std::time::Instant::now();
     loop {
-        if let Err(e) = refresh_screen(&mut drops, terminal_size, &mut next_time_spawn) {
+        if let Err(e) = refresh_screen(&mut drops, terminal_size, &mut stdout, &mut next_time_spawn)
+        {
             panic!("{}", e);
         }
 
@@ -62,30 +83,36 @@ fn main() {
 fn refresh_screen(
     drops: &mut Vec<Drop>,
     terminal_size: (u16, u16),
+    stdout: &mut RawTerminal<Stdout>,
     next_time_spawn: &mut std::time::Instant,
 ) -> Result<(), std::io::Error> {
     if std::time::Instant::now() >= *next_time_spawn {
         let mut rng = rand::thread_rng();
+        let length = rng.gen_range(5..=20);
         let x_pos = rng.gen_range(0..terminal_size.0) + 1;
         let speed = std::time::Duration::from_millis(rng.gen_range(100..=200));
         let characters = generate_character_vec(terminal_size.1, &mut rng);
 
         drops.push(Drop {
-            lenght: terminal_size.1,
+            length,
             x_pos,
+            y_offset: 0,
             speed,
-            next_time: std::time::Instant::now() + speed,
+            next_time: std::time::Instant::now(),
             characters,
         });
 
-        *next_time_spawn += std::time::Duration::from_millis(800);
+        *next_time_spawn += std::time::Duration::from_millis(100);
     }
 
-    for drop in drops.iter_mut() {
-        if std::time::Instant::now() >= drop.next_time {
-            drop.draw();
-            drop.reset_timer();
+    let mut index = 0;
+    while index < drops.len() {
+        drops[index].draw_if_timer_is_elapsed(stdout);
+        if drops[index].y_offset > terminal_size.1 {
+            drops.remove(index);
+            continue;
         }
+        index += 1;
     }
 
     Ok(())
